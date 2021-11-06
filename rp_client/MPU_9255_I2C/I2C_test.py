b708715b8fe1cc235bac25d3f4c76c04a732ccde
adam-wojewoda <adas.wojewoda@gmail.com>
2021-11-06 18:27:06 +0100

    Cleaned the code up

--- rp_client/MPU_9255_I2C/I2C_test.py
+++ rp_client/MPU_9255_I2C/I2C_test.py
@@ -1,20 +1,8 @@
-import smbus2
-import time
-bus = smbus2.SMBus(1)
-address = 0x68
+if __name__ == '__main__':
+    import smbus2
 
-#bear = bus.read_byte_data(address, 1)
+    bus = smbus2.SMBus(1)
+    address = 0x68
 
-
-
-for i in range(128):
-	
-	#bear255 = bearing255()      #this returns the value as a byte between 0 and 255. 
-	print (str(i)+': ' +str(bus.read_byte_data(address, i)))
-	#time.sleep(1)
-#while True:
-#	try:
-#	    print(bus.read_i2c_block_data(address, 59, 12))
-#	except TimeoutError:
-#		print('timeout')
-#		time.sleep(0.001)
+    for i in range(128):
+        print(str(i) + ': ' + str(bus.read_byte_data(address, i)))
