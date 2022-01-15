597e769be616ed34020672e201fb7d1296a2e5f4
adam-wojewoda <adas.wojewoda@gmail.com>
2022-01-15 20:12:41 +0100

    Written and tested tcp socket communication List of commands and messages should be taken to single shared file (to do)

--- gui_server/TCP_socket_server.py
+++ gui_server/TCP_socket_server.py
@@ -4,27 +4,103 @@
 #   2. Getting client state for gui to use
 
 import socket
+import threading
+from queue import Queue
+from time import sleep
 
 
-class TcpServer:
-    def __init__(self):
-        HOST = ''  # Symbolic name meaning all available interfaces
-        PORT = 50007  # Arbitrary non-privileged port
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((HOST, PORT))
-        s.listen(1)
-        conn, addr = s.accept()
-        print('Connected by', addr)
+class TcpServer(threading.Thread):
+    def __init__(self, commands_q: Queue, messages_q: Queue, events_in):
+        threading.Thread.__init__(self)
+        self.connection_threads = Queue()
+        self.events_list = events_in
+        self.commands_queue = commands_q
+        self.messages_queue = messages_q
+        self.host = ''  # Symbolic name meaning all available interfaces
+        self.port = 50007  # Arbitrary non-privileged port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.host, self.port))
 
+        """I know I should import these from some other file, but i'll do it later when it works properly"""
+        self.available_messages = {
+            'inf sensors error': b'sens_err',
+            'inf sensors ok': b'sens_ok',
+            'inf laser error': b'laser_err',
+            'inf laser ok': b'laser_ok',
+            'inf laser working': b'laser_working',
+            'inf laser waiting': b'laser_waiting',
+            'inf laser put down': b'laser_put_down',
+            'inf laser picked up': b'laser_picked_up',
+            'que get db name': b'get_db_name',
+            'que get command': b'get_command',  # <-------- very important one (aks what to do)
+        }
+        self.available_commands = {
+            'com start sensors': b'sens_start',
+            'com stop sensors': b'sens_stop',
+            'com start laser': b'laser_start',
+            'com stop laser': b'laser_stop',
+            'com get sensor status': b'get_sens_stat',
+            'com get laser status': b'get_laser_stat',
+            'com get laser working': b'get_laser_working',
+            'com new db name av': b'new_db_name_av',
+            'res message received': b'message_received',
+            'com carry on': b'carry_on'  # <-------- sent as info, that everything is ok
+        }
+
+    def operate_connection(self, conn_in):
+        print('new connection')
         while 1:
-            data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
-        conn.close()
+            data = conn_in.recv(1024)
+            # try to interpret the connection
 
+            if not data: break  # end of connection
+            conn_in.sendall(data)
+        conn_in.close()
+        pass
 
-# Test specific code
+    def close_threads(self):
+        print('threads closer started')
+        while not (self.events_list['stop_server'].is_set() and self.connection_threads.empty()):
+            if not self.connection_threads.empty():
+                tmp = self.connection_threads.get()
+                tmp.join()
+                print('thread closed')
+            sleep(0.1)
+
+    def run(self):
+        print('starting the server')
+        self.events_list['stop_server'].clear()
+        print('creating thread closer')
+        closer_thread = threading.Thread(target=self.close_threads)
+        print('starting thread closer')
+        closer_thread.start()
+        print('calling listen loop')
+        while not self.events_list['stop_server'].is_set():
+
+            self.s.settimeout(0.1)
+            try:
+                self.s.listen(1)
+                conn, addr = self.s.accept()
+                print('Connected by', addr)
+                t = threading.Thread(target=self.operate_connection, args=[conn])
+                t.start()
+                self.connection_threads.put(t)
+            except socket.timeout:
+                pass
+        print('no more listening, waiting for connections to close')
+        closer_thread.join()
+        print('server stopped')
+
 
 # Test calling
 if __name__ == '__main__':
-    print('Lack of testing code')
+    commands_q=Queue()
+    messages_q=Queue()
+    events_in={'stop_server': threading.Event()}
+    events_in['stop_server'].clear()
+
+    server = TcpServer(commands_q,messages_q,events_in)
+    server.start()
+    sleep(20)
+    print('calling stop command')
+    events_in['stop_server'].set()
--- rp_client/TCP_socket_client.py
+++ rp_client/TCP_socket_client.py
@@ -2,8 +2,94 @@
 # This code takes care of:
 #   1. Taking commands from main server for main process to take
 
-# Test specific code
+# Echo client program
+import socket
+
+
+class TCP_socket_client:
+    def __init__(self, host_in, port_in):
+        self.list_sockets = []  # [{message : xxx, socket : xxx}, ...]
+        self.host = host_in
+        self.port = port_in
+        self.available_messages = {
+            'inf sensors error': b'sens_err',
+            'inf sensors ok': b'sens_ok',
+            'inf laser error': b'laser_err',
+            'inf laser ok': b'laser_ok',
+            'inf laser working': b'laser_working',
+            'inf laser waiting': b'laser_waiting',
+            'inf laser put down': b'laser_put_down',
+            'inf laser picked up': b'laser_picked_up',
+            'que get db name': b'get_db_name',
+            'que get command': b'get_command',  # <-------- very important one (aks what to do)
+        }
+        self.available_commands = {
+            'com start sensors': b'sens_start',
+            'com stop sensors': b'sens_stop',
+            'com start laser': b'laser_start',
+            'com stop laser': b'laser_stop',
+            'com get sensor status': b'get_sens_stat',
+            'com get laser status': b'get_laser_stat',
+            'com get laser working': b'get_laser_working',
+            'com new db name av': b'new_db_name_av',
+            'res message received': b'message_received',
+            'com carry on': b'carry_on'  # <-------- sent as info, that everything is ok
+        }
+        pass
+
+    def send_message(self, message):
+        # create socket
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # connect my dear socket
+        soc.connect((self.host, self.port))
+        # send the message to server
+        soc.sendall(self.available_messages[message])
+        # add it to list of active sockets
+        self.list_sockets.append({'message': message, 'socket': soc})
+
+    def check_response(self):
+        # check if we have any sockets to read
+        if len(self.list_sockets) > 0:
+            response = self.list_sockets[0]['socket'].recv(1024)
+            message = self.list_sockets[0]['message']
+            self.list_sockets[0]['socket'].close()
+            del self.list_sockets[0]
+            return {'message': message, 'response': response}
+        else:
+            # no sockets to read, return None
+            return None
+
 
 # Test calling
 if __name__ == '__main__':
-    print('Lack of testing code')
+    # Make sure You have the test server running
+
+    def tester(sc, mess_in):
+        print('Sending message: ', mess_in)
+        socket_client.send_message(mess_in)
+        print('Server response: ', sc.check_response()['response'])
+        print(' ')
+
+
+    socket_client = TCP_socket_client('localhost', 50007)
+
+    # 1. 'inf sensors error': b'sens_err'
+    tester(socket_client, 'inf sensors error')
+    # 2. 'inf sensors ok': b'sens_ok'
+    tester(socket_client, 'inf sensors ok')
+    # 3. 'inf laser error': b'laser_err'
+    tester(socket_client, 'inf laser error')
+    # 4. 'inf laser ok': b'laser_ok'
+    tester(socket_client, 'inf laser ok')
+    # 5. 'inf laser working': b'laser_working'
+    tester(socket_client, 'inf laser working')
+    # 6. 'inf laser waiting': b'laser_waiting'
+    tester(socket_client, 'inf laser waiting')
+    # 7. 'inf laser put down': b'laser_put_down'
+    tester(socket_client, 'inf laser put down')
+    # 8. 'inf laser picked up': b'laser_picked_up'
+    tester(socket_client, 'inf laser picked up')
+    # 9. 'que get db name': b'get_db_name'
+    tester(socket_client, 'que get db name')
+    # 10.'que get command': b'get_command'
+    tester(socket_client, 'que get command')
